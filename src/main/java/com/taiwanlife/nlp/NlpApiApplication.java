0b687934a6caee9c47dc0283279c2b0fbd4b6957
henry_hsieh <henry_hsieh@bankpro.com.tw>
2022-02-24 22:31:19 +0800

    sit test 2

--- src/main/java/com/taiwanlife/nlp/NlpApiApplication.java
+++ src/main/java/com/taiwanlife/nlp/NlpApiApplication.java
@@ -11,7 +11,7 @@ public class NlpApiApplication {
 
     @GetMapping("/api/nlp")
     public String nlp() {
-        return "NLP API had been called!!! sit";
+        return "NLP API had been called!!! sit2";
     }
     
 	public static void main(String[] args) {
