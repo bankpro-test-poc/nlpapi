136b859e936eeaa72223223c0b68ad0d14bbf2d6
henry_hsieh <henry_hsieh@bankpro.com.tw>
2022-02-24 17:04:16 +0800

    初始匯入

---
+++ src/main/java/com/taiwanlife/nlp/NlpApiApplication.java
@@ -0,0 +1,21 @@
+package com.taiwanlife.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class NlpApiApplication {
+
+    @GetMapping("/api/nlp")
+    public String nlp() {
+        return "NLP API had been called!!!";
+    }
+    
+	public static void main(String[] args) {
+		SpringApplication.run(NlpApiApplication.class, args);
+	}
+
+}
